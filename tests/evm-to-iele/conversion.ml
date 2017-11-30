4428f1c05c69b57c8af2901d04627899882c44b0
Dwight Guth <dwight.guth@runtimeverification.com>
2017-11-29 18:53:31 -0600

    conversion.ml, iele.md: make divide by zero an exception

--- tests/evm-to-iele/conversion.ml
+++ tests/evm-to-iele/conversion.ml
@@ -807,6 +807,8 @@ let rec postprocess_iele iele label memcells = match iele with
 | Op(`SHA3, reg, [memstart; memwidth]) :: tl -> LiOp(`LOADPOS, -1, Z.zero) :: Op(`MLOADN, -2, [-1; memstart; memwidth]) :: LiOp(`LOADPOS, memstart, memcells) :: VoidOp(`MSTOREN, [memstart; -1; -2; memwidth]) :: Op(`SHA3, reg, [memstart]) :: postprocess_iele tl label (Z.add memcells Z.one)
 | VoidOp(`LOG(n), memstart :: memwidth :: topics) :: tl -> LiOp(`LOADPOS, -1, Z.zero) :: Op(`MLOADN, -2, [-1; memstart; memwidth]) :: LiOp(`LOADPOS, memstart, memcells) :: VoidOp(`MSTOREN, [memstart; -1; -2; memwidth]) :: VoidOp(`LOG(n), memstart :: topics) :: postprocess_iele tl label (Z.add memcells Z.one)
 | VoidOp(`STOP, []) :: tl -> LiOp(`LOADPOS, -1, Z.zero) :: VoidOp(`RETURN(1), [-1]) :: postprocess_iele tl label memcells
+| Op((`DIV|`MOD), reg, [v1;v2]) as op :: tl when compatibility -> VoidOp(`JUMPI(label), [v2]) :: LiOp(`LOADPOS, reg, Z.zero) :: VoidOp(`JUMP(label-1),[]) :: VoidOp(`JUMPDEST(label),[]) :: op :: VoidOp(`JUMPDEST(label-1),[]) :: postprocess_iele tl (label-2) memcells
+| Op((`ADDMOD|`MULMOD), reg, [v1;v2;v3]) as op :: tl when compatibility -> VoidOp(`JUMPI(label), [v3]) :: LiOp(`LOADPOS, reg, Z.zero) :: VoidOp(`JUMP(label-1),[]) :: VoidOp(`JUMPDEST(label),[]) :: op :: VoidOp(`JUMPDEST(label-1),[]) :: postprocess_iele tl (label-2) memcells
 | hd :: tl -> hd :: postprocess_iele tl label memcells
 | [] -> []
 
