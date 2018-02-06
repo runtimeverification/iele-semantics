577304bc24a16b6358169e46fe7673cbcf0f47e6
Dwight Guth <dwight.guth@runtimeverification.com>
2018-02-06 14:50:46 -0600

    ieleTestClient.ml: fail on failing assertions

--- iele-vm/ieleTestClient.ml
+++ iele-vm/ieleTestClient.ml
@@ -2,10 +2,12 @@ open Yojson
 open Yojson.Basic.Util
 open Msg_types
 
+let file = Sys.argv.(1)
+
 let () = if Array.length Sys.argv <> 2 then 
   prerr_endline ("usage: " ^ Sys.argv.(0) ^ " <file.iele.json>")
 
-let json = Yojson.Basic.from_channel (open_in Sys.argv.(1))
+let json = Yojson.Basic.from_channel (open_in file)
 
 let context = Secp256k1.Context.create [Secp256k1.Context.Sign; Secp256k1.Context.Verify]
 let hash () = Cryptokit.Hash.keccak 256
@@ -17,6 +19,8 @@ let test =
 
 let sort l =
   List.sort (fun (a,_) (b,_) -> compare a b) l
+
+let failed = ref false
 
 let of_hex signed str =
   if str = "" then Bytes.empty else
@@ -172,22 +176,32 @@ let test_transaction header (state: (string * Basic.json) list) (tx: Basic.json)
   let expected_return = List.map (fun json -> of_hex (json |> to_string)) (result |> member "out" |> to_list) in
   let rets = unpack_output call_result.return_data in
   let actual_return = List.map (fun arg -> `String (Bytes.to_string arg)) rets in
-  if expected_return <> rets then
-    prerr_endline ("failed: out:\n" ^ Yojson.Basic.to_string (`List actual_return));
+  if expected_return <> rets then begin
+    prerr_endline ("failed " ^ file ^ ": out:\n" ^ Yojson.Basic.to_string (`List actual_return));
+    failed := true;
+  end;
   let expected_returncode = of_hex (result |> member "status" |> to_string) in
-  if World.to_z expected_returncode <> World.to_z call_result.return_code then
-    prerr_endline ("failed: status:\n" ^ (to_hex call_result.return_code));
+  if World.to_z expected_returncode <> World.to_z call_result.return_code then begin
+    prerr_endline ("failed " ^ file ^ ": status:\n" ^ (to_hex call_result.return_code));
+    failed := true;
+  end;
   let expected_gas = of_hex (result |> member "gas" |> to_string) in
-  if World.to_z expected_gas <> World.to_z call_result.gas_remaining then
-    prerr_endline ("failed: gas:\n" ^ (to_hex call_result.gas_remaining));
+  if World.to_z expected_gas <> World.to_z call_result.gas_remaining then begin
+    prerr_endline ("failed " ^ file ^ ": gas:\n" ^ (to_hex call_result.gas_remaining));
+    failed := true;
+  end;
   let expected_refund = of_hex (result |> member "refund" |> to_string) in
-  if World.to_z expected_refund <> World.to_z call_result.gas_refund then
-    prerr_endline ("failed: refund:\n" ^ (to_hex call_result.gas_refund));
+  if World.to_z expected_refund <> World.to_z call_result.gas_refund then begin
+    prerr_endline ("failed " ^ file ^ ": refund:\n" ^ (to_hex call_result.gas_refund));
+    failed := true;
+  end;
   let expected_logs = of_hex_unsigned (result |> member "logs" |> to_string) in
   let rlp_logs = Rlp.RlpList(List.map log_to_rlp call_result.logs) in
   let actual_logs = Bytes.of_string (Cryptokit.hash_string (hash()) (Rope.to_string (Rlp.encode rlp_logs))) in
-  if expected_logs <> actual_logs then
-    prerr_endline ("failed: logs:\nactual: " ^ (Rlp.display (rlp_to_hex rlp_logs)) ^ "\nhash: " ^ (to_hex actual_logs));
+  if expected_logs <> actual_logs then begin
+    prerr_endline ("failed " ^ file ^ ": logs:\nactual: " ^ (Rlp.display (rlp_to_hex rlp_logs)) ^ "\nhash: " ^ (to_hex actual_logs));
+    failed := true;
+  end;
   update_state checkpoint_state call_result.modified_accounts call_result.deleted_accounts
 
 let rec canonicalize assoc =
@@ -215,6 +229,9 @@ let json_blockhashes = test |> member "blockhashes" |> to_list
 let str_blockhashes = List.map to_string json_blockhashes
 let blockhashes = List.map of_hex str_blockhashes;;
 List.iter World.InMemoryWorldState.add_blockhash (List.rev blockhashes);;
-let actual = sort (canonicalize (List.fold_left test_block pre blocks))
-let () = if expected <> actual then
-  prerr_endline ("failed: postState:\nexpected:" ^ Yojson.Basic.to_string (`Assoc expected) ^ "\nactual: " ^ Yojson.Basic.to_string (`Assoc actual))
+let actual = sort (canonicalize (List.fold_left test_block pre blocks));;
+if expected <> actual then begin
+  prerr_endline ("failed " ^ file ^ ": postState:\nexpected:" ^ Yojson.Basic.to_string (`Assoc expected) ^ "\nactual: " ^ Yojson.Basic.to_string (`Assoc actual));
+  failed := true;
+end;;
+if !failed then failwith "assertions failed";;
