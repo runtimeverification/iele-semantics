6a858a4270b73674358862b0821a160f67b19fb3
Dwight Guth <dwight.guth@runtimeverification.com>
2018-02-08 13:18:15 -0600

    caching.ml: thread-local request caching

--- iele-semantics-plugin/caching.ml
+++ iele-semantics-plugin/caching.ml
@@ -17,10 +17,7 @@ end
 module IntHash = Hashtbl.Make(Z)
 
 module Make ( W : World.WorldState ) : KWorldState = struct
-  let blockhashes = IntHash.create 10
-  let accounts = IntHash.create 10
-  let codes = IntHash.create 10
-  let storages = IntHash.create 10
+  let cache = IntHash.create 10
 
   let getOrUpdate map key default =
     try
@@ -30,10 +27,19 @@ module Make ( W : World.WorldState ) : KWorldState = struct
       IntHash.add map key result;
       result
 
+  let thread_id () = Z.of_int (Thread.id (Thread.self()))
+
+  let getOrUpdateLocal select key default =
+    getOrUpdate (select (getOrUpdate cache (thread_id ()) (fun () -> (IntHash.create 10,IntHash.create 10,IntHash.create 10,IntHash.create 10)))) key default
+
+  let accounts    (a,_,_,_) = a
+  let storages    (_,s,_,_) = s
+  let codes       (_,_,c,_) = c
+  let blockhashes (_,_,_,b) = b
+
   let get_account acct =
-    getOrUpdate accounts acct (fun () -> 
+    getOrUpdateLocal accounts acct (fun () -> 
       let data = W.get_account (of_z acct) in
-      if data.code_empty then IntHash.add codes acct "";
       data)
 
   let get_balance acct = to_z (get_account acct).balance
@@ -41,18 +47,15 @@ module Make ( W : World.WorldState ) : KWorldState = struct
   let is_code_empty acct = (get_account acct).code_empty
 
   let get_storage_data acct index =
-    let map = getOrUpdate storages acct (fun () -> IntHash.create 10) in
+    let map = getOrUpdateLocal storages acct (fun () -> IntHash.create 10) in
     getOrUpdate map index (fun () -> to_z (W.get_storage_data (of_z acct) (of_z index)))
        
   let get_code acct =
-    getOrUpdate codes acct (fun () -> Bytes.to_string (W.get_code (of_z acct)))
+    getOrUpdateLocal codes acct (fun () -> Bytes.to_string (W.get_code (of_z acct)))
 
   let get_blockhash offset =
-    getOrUpdate blockhashes offset (fun () -> to_z (W.get_blockhash (Z.to_int offset)))
+    getOrUpdateLocal blockhashes offset (fun () -> to_z (W.get_blockhash (Z.to_int offset)))
 
   let clear () =
-    IntHash.clear blockhashes;
-    IntHash.clear accounts;
-    IntHash.clear codes;
-    IntHash.clear storages
+    IntHash.remove cache (thread_id ())
 end
