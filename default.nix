bc6598a1d8ec376abaa606ab95e5dd236fba264b
Thomas Tuegel <ttuegel@mailbox.org>
2020-11-20 13:24:40 -0600

    Add Nix expression

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ checkMaterialization ? false
+}:
+
+let
+  sources = import ./nix/sources.nix;
+
+  pkgs =
+    let
+      haskell-nix = import sources."haskell.nix" {};
+      inherit (haskell-nix) nixpkgsArgs;
+      args = nixpkgsArgs // { };
+    in import haskell-nix.sources.nixpkgs-2003 args;
+  inherit (pkgs) lib haskell-nix;
+
+  iele-assemble-project = (args: haskell-nix.stackProject args) {
+    src = haskell-nix.haskellLib.cleanGit { src = ./.; subDir = "compiler"; };
+  };
+
+  default =
+    {
+      inherit pkgs iele-assemble-project;
+      inherit (iele-assemble-project.compiler.components.exes) iele-assemble;
+      cache = [
+        iele-assemble-project.roots
+      ];
+    };
+
+in default
