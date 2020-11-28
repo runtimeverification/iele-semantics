862a2a29d056b475d7eca8f809365a66d0682950
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-11-28 17:35:53 +0000

    package/debian/build-package: setup opam

--- package/debian/build-package.sh
+++ package/debian/build-package.sh
@@ -9,6 +9,7 @@ K_RELEASE="https://github.com/kframework/k/releases/download/v5.0.0-${K_SHORT_RE
 curl --fail --location "${K_RELEASE}/kframework_5.0.0_amd64_${UBUNTU_RELEASE}.deb" --output kframework-${UBUNTU_RELEASE}.deb
 sudo apt-get update && sudo apt-get upgrade --yes
 sudo apt-get install --yes ./kframework-${UBUNTU_RELEASE}.deb
+sudo bash -c 'OPAMROOT=/usr/lib/kframework/opamroot k-configure-opam'
 cp -r package/debian ./
 dpkg-buildpackage
 mv ../kiele_${KIELE_VERSION}_amd64.deb ../kiele_${KIELE_VERSION}_amd64_${UBUNTU_RELEASE}.deb
