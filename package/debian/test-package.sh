7b35459ad4f7de4e078bfe3862821350663f5bc1
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-12-02 18:55:41 +0000

    package/debian: remove calls to opam and such that shouldnt be needed now

--- package/debian/test-package.sh
+++ package/debian/test-package.sh
@@ -10,14 +10,8 @@ K_SHORT_REV="$4"
 K_RELEASE="https://github.com/kframework/k/releases/download/v5.0.0-${K_SHORT_REV}"
 
 sudo apt-get update && sudo apt-get upgrade --yes
-sudo apt-get install --yes opam netcat
-curl --fail --location "${K_RELEASE}/kframework_5.0.0_amd64_${UBUNTU_RELEASE}.deb" --output kframework-${UBUNTU_RELEASE}.deb
-sudo apt-get install --yes ./kframework-${UBUNTU_RELEASE}.deb
+sudo apt-get install --yes netcat
 sudo apt-get install --yes ./kiele_${KIELE_VERSION}_amd64_${UBUNTU_RELEASE}.deb
-sudo bash -c 'OPAMROOT=/usr/lib/kframework/opamroot k-configure-opam'
-sudo bash -c 'OPAMROOT=/usr/lib/kframework/opamroot opam install --yes ocaml-protoc rlp yojson zarith hex uuidm cryptokit'
-export OPAMROOT=/usr/lib/kframework/opamroot
-eval $(opam config env)
 
 git clone 'https://github.com/runtimeverification/iele-semantics'
 cd iele-semantics
