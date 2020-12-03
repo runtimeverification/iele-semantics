e68886a48d31d2efcb097c3d5b7d3db92a0081b3
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-03 16:00:07 -0600

    Simplify Nix expression hierarchy

--- default.nix
+++ default.nix
@@ -1,30 +1,11 @@
-{ checkMaterialization ? false
-}:
+{}:
 
 let
-  sources = import ./nix/sources.nix;
-
-  pkgs =
-    let
-      haskell-nix = import sources."haskell.nix" {};
-      inherit (haskell-nix) nixpkgsArgs;
-      args = nixpkgsArgs // { };
-    in import haskell-nix.sources.nixpkgs-2003 args;
-  inherit (pkgs) lib haskell-nix;
-
-  iele-assemble-project = (args: haskell-nix.stackProject args) {
-    inherit checkMaterialization;
-    materialized = ./nix/iele-assemble.materialized;
-    src = haskell-nix.haskellLib.cleanGit { src = ./.; subDir = "iele-assemble"; };
-  };
+  iele-assemble = import ./iele-assemble {};
 
   default =
     {
-      inherit pkgs iele-assemble-project;
-      inherit (iele-assemble-project.iele-assemble.components.exes) iele-assemble;
-      cache = [
-        iele-assemble-project.roots
-      ];
+      inherit (iele-assemble) iele-assemble
     };
 
 in default
--- iele-assemble/default.nix
+++ iele-assemble/default.nix
@@ -1,1 +1,27 @@
-import ../default.nix
+{ checkMaterialization ? false
+}:
+
+let
+  sources = import ../nix/sources.nix;
+
+  pkgs =
+    let
+      haskell-nix = import sources."haskell.nix" {};
+      inherit (haskell-nix) nixpkgsArgs;
+      args = nixpkgsArgs // { };
+    in import haskell-nix.sources.nixpkgs-2003 args;
+  inherit (pkgs) lib haskell-nix;
+
+  project = (args: haskell-nix.stackProject args) {
+    inherit checkMaterialization;
+    materialized = ../nix/iele-assemble.nix.d;
+    src = haskell-nix.haskellLib.cleanGit { src = ./..; subDir = "iele-assemble"; };
+  };
+
+  default =
+    {
+      inherit pkgs project;
+      inherit (project.iele-assemble.components.exes) iele-assemble;
+    };
+
+in default
