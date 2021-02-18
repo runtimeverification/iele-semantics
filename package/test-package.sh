b35400700e15b9190785a1231a3194785dd27085
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2021-02-18 15:07:43 +0000

    package: make separate test-package script

---
+++ package/test-package.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+KIELE_REVISION="$1" ; shift
+TEST_PORT="$1"      ; shift
+
+git clone 'https://github.com/runtimeverification/iele-semantics'
+cd iele-semantics
+git checkout "$KIELE_REVISION"
+git submodule update --init --recursive
+
+make test-vm -j4
+make test-iele -j4
+
+kiele vm --port ${TEST_PORT} &
+pid=$!
+sleep 3
+make test-iele-node  -j4 TEST_PORT=${TEST_PORT}
+make test-bad-packet -j4 TEST_PORT=${TEST_PORT}
+kill $pid
