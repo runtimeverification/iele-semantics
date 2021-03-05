637aa766f2f8ee2c4d7c9d36166f555e05674865
RV Jenkins <admin@runtimeverification.com>
2021-03-05 05:53:34 +0000

    gh-pages: Updated the website

--- nix/libff.nix
+++
@@ -1,8 +0,0 @@
-{ stdenv, cmake, pkgconfig, gmp, openssl, procps }:
-
-stdenv.mkDerivation {
-  name = "libff";
-  src = ../plugin/deps/libff;
-  nativeBuildInputs = [ cmake pkgconfig ];
-  propagatedBuildInputs = [ gmp openssl procps ];
-}
